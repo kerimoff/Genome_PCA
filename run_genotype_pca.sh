eaef9ee80533000c49ef0cd6a82b88f6aead43c1
kerimoff <kerimov.nurlan@gmail.com>
2019-09-04 13:21:26 +0300

    update running script

--- run_genotype_pca.sh
+++ run_genotype_pca.sh
@@ -1,5 +1,7 @@
 nextflow run nextflow_pca_map.nf\
  -profile tartu_hpc\
- --vcf /gpfs/hpc/home/a72094/datasets/controlled_access/ImmVar/genotypes/Michigan_GRCh37_1KGPhase3_150119/GRCh38/ImmVar_GRCh38.filtered.vcf.gz\
- --data_name ImmVar\
+ --vcf /gpfs/hpc/home/a72094/datasets/controlled_access/BrainSeq/genotypes/Michigan_GRCh37_Phase3_250819/merged/BrainSeq_GRCh38_filtered.vcf.gz\
+ --data_name BrainSeq\
+ --outdir /gpfs/hpc/home/kerimov/Genome_QC_Dev/results_BrainSeq/\
  -resume
+
